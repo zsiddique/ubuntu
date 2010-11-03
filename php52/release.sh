c699a546adad1be45e6fa16e24f4484d65170d7e
Sascha Kettler <kettler@gmx.net>
2010-11-03 13:18:32 +0100

    Fixed debian/rules

--- php52/release.sh
+++ php52/release.sh
@@ -3,14 +3,27 @@
 # Set to "-sa" when the orig.tar.bz2 needs to be uploaded
 DEBUILD_PARAMETER="-sa"
 PPA_REPOSITORY="ppa:skettler/php"
+GIT_UBUNTU_RELEASE="lucid"
 
 PACKAGE=`basename "${PWD}"`
 DIRECTORY=`ls -1d "${PACKAGE}"-* | tail -1`
 PROGRAM_VERSION=`echo "${DIRECTORY}" | sed "s/${PACKAGE}-//"`
 PACKAGE_VERSION=`cd "${DIRECTORY}"; dpkg-parsechangelog|grep "^Version: "|sed "s/^Version: //"`
 
+. /etc/lsb-release
+
+( cd ${DIRECTORY} && ./debian/rules clean ); [ $? -eq 0 ]
+
+if [ "${DISTRIB_CODENAME}" != "${GIT_UBUNTU_RELEASE}" ]; then
+    sed -i "s~${GIT_UBUNTU_RELEASE}~${DISTRIB_CODENAME}~g" ${DIRECTORY}/debian/changelog
+fi
+
 if [ ! -e ${PACKAGE}_${PACKAGE_VERSION}_source.changes ]; then
     ( cd ${DIRECTORY} && debuild -S ${DEBUILD_PARAMETER} ); [ $? -eq 0 ]
     dput ${PPA_REPOSITORY} ${PACKAGE}_${PACKAGE_VERSION}_source.changes
+fi
+
+if [ "${DISTRIB_CODENAME}" != "${GIT_UBUNTU_RELEASE}" ]; then
+    sed -i "s~${DISTRIB_CODENAME}~${GIT_UBUNTU_RELEASE}~g" ${DIRECTORY}/debian/changelog
 fi
 
