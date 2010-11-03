9d7afcb4ed7559584506eda61a7092702a2400e6
Sascha Kettler <kettler@gmx.net>
2010-11-03 19:22:38 +0100

    Updated release helper script

--- tools/rrsync/release.sh
+++ tools/rrsync/release.sh
@@ -22,6 +22,7 @@ PACKAGE_VERSION=`cd "${DIRECTORY}"; dpkg-parsechangelog|grep "^Version: "|sed "s
 ( cd ${DIRECTORY} && debuild -S ${DEBUILD_PARAMETER} ); [ $? -eq 0 ]
 dput ${PPA_REPOSITORY}/${PPA_DIRECTORY} ${PACKAGE}_${PACKAGE_VERSION}_source.changes
 rm -f ${PACKAGE}_${PACKAGE_VERSION}.dsc ${PACKAGE}_${PACKAGE_VERSION}_source.*
+rm -f ${PACKAGE}_${PACKAGE_VERSION}.tar.gz ${PACKAGE}_${PACKAGE_VERSION}.tar.bz2
 
 if [ "${DISTRIB_CODENAME}" != "${GIT_UBUNTU_RELEASE}" ]; then
     sed -i "s~${DISTRIB_CODENAME}~${GIT_UBUNTU_RELEASE}~g" ${DIRECTORY}/debian/changelog
