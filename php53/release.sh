93f3e5840ea10f15fd46df8dbee05bfbec59ec4b
Sascha Kettler <kettler@gmx.net>
2010-11-03 14:52:16 +0100

    Commit unsaved work

--- php53/release.sh
+++ php53/release.sh
@@ -8,7 +8,6 @@ GIT_UBUNTU_RELEASE="lucid"
 PACKAGE=`basename "${PWD}"`
 DIRECTORY=`ls -1d "${PACKAGE}"-* | tail -1`
 PROGRAM_VERSION=`echo "${DIRECTORY}" | sed "s/${PACKAGE}-//"`
-PACKAGE_VERSION=`cd "${DIRECTORY}"; dpkg-parsechangelog|grep "^Version: "|sed "s/^Version: //"`
 
 . /etc/lsb-release
 
@@ -18,6 +17,7 @@ if [ "${DISTRIB_CODENAME}" != "${GIT_UBUNTU_RELEASE}" ]; then
     sed -i "s~${GIT_UBUNTU_RELEASE}~${DISTRIB_CODENAME}~g" ${DIRECTORY}/debian/changelog
 fi
 
+PACKAGE_VERSION=`cd "${DIRECTORY}"; dpkg-parsechangelog|grep "^Version: "|sed "s/^Version: //"`
 if [ ! -e ${PACKAGE}_${PACKAGE_VERSION}_source.changes ]; then
     ( cd ${DIRECTORY} && debuild -S ${DEBUILD_PARAMETER} ); [ $? -eq 0 ]
     dput ${PPA_REPOSITORY} ${PACKAGE}_${PACKAGE_VERSION}_source.changes
