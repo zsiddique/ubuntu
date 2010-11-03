d6c8ef6283ba125869a0459852b4424b76b673c4
Sascha Kettler <kettler@gmx.net>
2010-11-03 16:11:34 +0100

    Reenabled lpia build

--- php/php53/release.sh
+++ php/php53/release.sh
@@ -19,10 +19,9 @@ if [ "${DISTRIB_CODENAME}" != "${GIT_UBUNTU_RELEASE}" ]; then
 fi
 
 PACKAGE_VERSION=`cd "${DIRECTORY}"; dpkg-parsechangelog|grep "^Version: "|sed "s/^Version: //"`
-if [ ! -e ${PACKAGE}_${PACKAGE_VERSION}_source.changes ]; then
-    ( cd ${DIRECTORY} && debuild -S ${DEBUILD_PARAMETER} ); [ $? -eq 0 ]
-    dput ${PPA_REPOSITORY}/${PPA_DIRECTORY} ${PACKAGE}_${PACKAGE_VERSION}_source.changes
-fi
+( cd ${DIRECTORY} && debuild -S ${DEBUILD_PARAMETER} ); [ $? -eq 0 ]
+dput ${PPA_REPOSITORY}/${PPA_DIRECTORY} ${PACKAGE}_${PACKAGE_VERSION}_source.changes
+rm -f ${PACKAGE}_${PACKAGE_VERSION}.dsc ${PACKAGE}_${PACKAGE_VERSION}_source.*
 
 if [ "${DISTRIB_CODENAME}" != "${GIT_UBUNTU_RELEASE}" ]; then
     sed -i "s~${DISTRIB_CODENAME}~${GIT_UBUNTU_RELEASE}~g" ${DIRECTORY}/debian/changelog
