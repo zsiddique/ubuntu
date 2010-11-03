22bb61e7e24cb114415e573c203ec21704aa3c48
Sascha Kettler <kettler@gmx.net>
2010-11-03 14:53:15 +0100

    Restructured repository

--- php/php52/release.sh
+++ php/php52/release.sh
@@ -2,9 +2,10 @@
 
 # Set to "-sa" when the orig.tar.bz2 needs to be uploaded
 DEBUILD_PARAMETER="-sa"
-PPA_REPOSITORY="ppa:skettler/php"
+PPA_REPOSITORY="ppa:skettler"
 GIT_UBUNTU_RELEASE="lucid"
 
+PPA_DIRECTORY=`cd .. && basename "${PWD}"`
 PACKAGE=`basename "${PWD}"`
 DIRECTORY=`ls -1d "${PACKAGE}"-* | tail -1`
 PROGRAM_VERSION=`echo "${DIRECTORY}" | sed "s/${PACKAGE}-//"`
@@ -20,7 +21,7 @@ fi
 PACKAGE_VERSION=`cd "${DIRECTORY}"; dpkg-parsechangelog|grep "^Version: "|sed "s/^Version: //"`
 if [ ! -e ${PACKAGE}_${PACKAGE_VERSION}_source.changes ]; then
     ( cd ${DIRECTORY} && debuild -S ${DEBUILD_PARAMETER} ); [ $? -eq 0 ]
-    dput ${PPA_REPOSITORY} ${PACKAGE}_${PACKAGE_VERSION}_source.changes
+    dput ${PPA_REPOSITORY}/${PPA_DIRECTORY} ${PACKAGE}_${PACKAGE_VERSION}_source.changes
 fi
 
 if [ "${DISTRIB_CODENAME}" != "${GIT_UBUNTU_RELEASE}" ]; then
